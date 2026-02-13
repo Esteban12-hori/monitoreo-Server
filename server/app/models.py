6d57f1c69e57b0312fa3d1f7a0777eb787f1ab1c
Joaquin Esteban Guajardo Núñez <joaquinestebanguajardonunez@MacBook-Air-de-Joaquin.local>
2026-02-13 16:03:55 -0300

    Fix User model declaration for imports

--- server/app/models.py
+++ server/app/models.py
@@ -102,7 +102,7 @@ class NotificationSettings(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
 
-
+class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
